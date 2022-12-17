1aac7f29d398fcce22804301fa97e87a73e7bd0a
sitegui <sitegui@sitegui.com.br>
2022-12-17 16:57:39 +0100

    Solve day 11

--- src/day_11.rs
+++ src/day_11.rs
@@ -6,8 +6,8 @@ use itertools::Itertools;
 use std::cell::RefCell;
 
 #[derive(Debug, Clone)]
-struct Monkey {
-    items: Vec<i64>,
+struct Monkey<I> {
+    items: Vec<I>,
     inspection: Inspection,
     test_divisible: i64,
     if_true: usize,
@@ -22,8 +22,19 @@ enum Inspection {
     Square,
 }
 
+#[derive(Debug, Clone)]
+struct RemainderItem {
+    remainder_values: Vec<RemainderValue>,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct RemainderValue {
+    module: i64,
+    remainder: i64,
+}
+
 pub fn solve(data: &Data) -> DayOutput {
-    let monkeys = data
+    let monkeys_part_1 = data
         .paragraphs()
         .enumerate()
         .map(|(i, lines)| {
@@ -34,7 +45,10 @@ pub fn solve(data: &Data) -> DayOutput {
             assert_eq!(header, format!("Monkey {}:", i).as_bytes());
 
             starting_items.consume_prefix(b"  Starting items: ");
-            let items = starting_items.split_bytes(b", ", false).parsed().collect();
+            let items = starting_items
+                .split_bytes(b", ", false)
+                .parsed::<i64>()
+                .collect();
 
             operation.consume_prefix(b"  Operation: new = old ");
             let inspection = match operation.consume_bytes(2) {
@@ -66,24 +80,45 @@ pub fn solve(data: &Data) -> DayOutput {
         })
         .collect_vec();
 
-    let part_1 = do_rounds(monkeys.clone(), 20, 3);
-    let part_2 = do_rounds(monkeys, 10_000, 1);
+    let modules = monkeys_part_1
+        .iter()
+        .map(|monkey| monkey.borrow().test_divisible)
+        .collect_vec();
+    let monkeys_part_2 = monkeys_part_1
+        .iter()
+        .map(|monkey| {
+            let monkey = monkey.borrow();
+            RefCell::new(Monkey {
+                items: monkey
+                    .items
+                    .iter()
+                    .map(|&item| RemainderItem {
+                        remainder_values: modules
+                            .iter()
+                            .map(|&module| RemainderValue {
+                                module,
+                                remainder: item % module,
+                            })
+                            .collect(),
+                    })
+                    .collect(),
+                inspection: monkey.inspection,
+                test_divisible: monkey.test_divisible,
+                if_true: monkey.if_true,
+                if_false: monkey.if_false,
+                business: monkey.business,
+            })
+        })
+        .collect_vec();
+
+    let part_1 = do_rounds_part_1(monkeys_part_1, 20, 3);
+    let part_2 = do_rounds_part_2(monkeys_part_2, 10_000);
 
     (part_1, part_2).into()
 }
 
-fn do_rounds(monkeys: Vec<RefCell<Monkey>>, num_rounds: i32, dumping: i64) -> i64 {
-    for i in 0..num_rounds {
-        if i % 1000 == 1 {
-            eprintln!(
-                "business = {}",
-                monkeys
-                    .iter()
-                    .map(|monkey| monkey.borrow().business)
-                    .format(", ")
-            );
-        }
-
+fn do_rounds_part_1(monkeys: Vec<RefCell<Monkey<i64>>>, num_rounds: i32, dumping: i64) -> i64 {
+    for _ in 0..num_rounds {
         for monkey in &monkeys {
             let mut monkey = monkey.borrow_mut();
             let monkey = &mut *monkey;
@@ -112,6 +147,47 @@ fn do_rounds(monkeys: Vec<RefCell<Monkey>>, num_rounds: i32, dumping: i64) -> i6
         .rev()
         .collect_vec();
 
-    eprintln!("business = {:?}", business);
+    (business[0] * business[1]) as i64
+}
+
+fn do_rounds_part_2(monkeys: Vec<RefCell<Monkey<RemainderItem>>>, num_rounds: i32) -> i64 {
+    for _ in 0..num_rounds {
+        for monkey in &monkeys {
+            let mut monkey = monkey.borrow_mut();
+            let monkey = &mut *monkey;
+            monkey.business += monkey.items.len();
+            for mut item in monkey.items.drain(..) {
+                let mut test_remainder = None;
+                for remainder in &mut item.remainder_values {
+                    let new_remainder = match monkey.inspection {
+                        Inspection::Add(n) => remainder.remainder + n,
+                        Inspection::Multiply(n) => remainder.remainder * n,
+                        Inspection::Square => remainder.remainder * remainder.remainder,
+                    };
+                    remainder.remainder = new_remainder % remainder.module;
+
+                    if remainder.module == monkey.test_divisible {
+                        test_remainder = Some(remainder.remainder);
+                    }
+                }
+
+                let target = if test_remainder.unwrap() == 0 {
+                    monkey.if_true
+                } else {
+                    monkey.if_false
+                };
+
+                monkeys[target].borrow_mut().items.push(item);
+            }
+        }
+    }
+
+    let business = monkeys
+        .iter()
+        .map(|monkey| monkey.borrow().business)
+        .sorted()
+        .rev()
+        .collect_vec();
+
     (business[0] * business[1]) as i64
 }
