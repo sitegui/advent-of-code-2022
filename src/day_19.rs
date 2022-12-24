5e6298af4386e48c9341aa0d9109e3074fe701d5
sitegui <sitegui@sitegui.com.br>
2022-12-24 18:04:59 +0100

    Day 19

--- src/day_19.rs
+++ src/day_19.rs
@@ -1,12 +1,9 @@
 use crate::data::Data;
 use crate::nom_parser::*;
-use crate::xy::Xyz;
 use crate::DayOutput;
-use derive_more::{Add, Sum};
-use ndarray::Array3;
-use std::collections::VecDeque;
+use derive_more::{Add, Sub, Sum};
 
-#[derive(Debug, Copy, Clone, Add, Sum)]
+#[derive(Debug, Copy, Clone)]
 enum Resource {
     Ore,
     Clay,
@@ -14,7 +11,7 @@ enum Resource {
     Geode,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Add, Sum, Sub)]
 struct Resources {
     ore: i32,
     clay: i32,
@@ -22,57 +19,94 @@ struct Resources {
     geode: i32,
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Robot {
-    cost: Resources,
-    kind: Resource,
-}
-
 #[derive(Debug, Clone)]
 struct Blueprint {
     id: i32,
-    robots: Vec<Robot>,
+    ore_robot_cost: Resources,
+    clay_robot_cost: Resources,
+    obsidian_robot_cost: Resources,
+    geode_robot_cost: Resources,
+    max_ore_robots: i32,
+    max_clay_robots: i32,
+    max_obsidian_robots: i32,
 }
 
-pub fn solve(data: &Data) -> DayOutput {}
+pub fn solve(data: &Data) -> DayOutput {
+    let blueprints = lines(parse_blueprint).consume_all(data.bytes());
+
+    let mut part_1 = 0;
+    let initial_robot_counts = Resources {
+        ore: 1,
+        clay: 0,
+        obsidian: 0,
+        geode: 0,
+    };
+    for blueprint in &blueprints {
+        let geodes = search(blueprint, Resources::ZERO, initial_robot_counts, 24, &mut 0);
+
+        part_1 += blueprint.id * geodes;
+    }
+
+    let mut part_2 = 1;
+    for blueprint in blueprints.iter().take(3) {
+        let geodes = search(blueprint, Resources::ZERO, initial_robot_counts, 32, &mut 0);
+
+        part_2 *= geodes as i64;
+    }
+
+    (part_1 as i64, part_2).into()
+}
 
 fn parse_blueprint(input: &[u8]) -> PResult<Blueprint> {
-    // Blueprint 4: Each ore robot costs 4 ore.
-    // Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 15 clay.
-    // Each geode robot costs 4 ore and 20 obsidian.
     map(
         tuple((
             tag(b"Blueprint "),
             nom_i32,
-            tag(b": Each ore robot costs "),
-            nom_i32,
-            tag(b" ore. Each clay robot costs "),
-            nom_i32,
-            tag(b" ore. Each obsidian robot costs "),
-            nom_i32,
-            tag(b" ore and "),
-            nom_i32,
-            tag(b" clay. Each geode robot costs "),
-            nom_i32,
-            tag(b" ore and "),
-            nom_i32,
-            tag(b" obsidian."),
+            tag(b": "),
+            parse_robot(b"ore"),
+            tag(b" "),
+            parse_robot(b"clay"),
+            tag(b" "),
+            parse_robot(b"obsidian"),
+            tag(b" "),
+            parse_robot(b"geode"),
         )),
-        || {},
-    )
+        |(
+            _,
+            id,
+            _,
+            ore_robot_cost,
+            _,
+            clay_robot_cost,
+            _,
+            obsidian_robot_cost,
+            _,
+            geode_robot_cost,
+        )| {
+            Blueprint::new(
+                id,
+                ore_robot_cost,
+                clay_robot_cost,
+                obsidian_robot_cost,
+                geode_robot_cost,
+            )
+        },
+    )(input)
 }
 
-fn parse_robot_cost(input: &[u8]) -> PResult<Robot> {
-    map(
-        tuple((
-            tag(b"Each "),
-            parse_resource,
-            tag(b" robot costs "),
-            tag(b" "),
-            tag(b"."),
-        )),
-        || {},
-    )(input)
+fn parse_robot<'a>(resource_name: &[u8]) -> impl PParser<'a, Resources> + '_ {
+    move |input| {
+        map(
+            tuple((
+                tag(b"Each "),
+                tag(resource_name),
+                tag(b" robot costs "),
+                parse_costs,
+                tag(b"."),
+            )),
+            |(_, _, _, cost, _)| cost,
+        )(input)
+    }
 }
 
 fn parse_costs(input: &[u8]) -> PResult<Resources> {
@@ -101,6 +135,125 @@ fn parse_resource(input: &[u8]) -> PResult<Resource> {
     ))(input)
 }
 
+fn search(
+    blueprint: &Blueprint,
+    resources: Resources,
+    robot_counts: Resources,
+    remaining_time: i32,
+    best_geodes_so_far: &mut i32,
+) -> i32 {
+    if remaining_time == 0 {
+        if resources.geode > *best_geodes_so_far {
+            *best_geodes_so_far = resources.geode;
+        }
+        return resources.geode;
+    }
+
+    // Bound branches provably will not be better than the best so far
+    let triangle_number = remaining_time * (remaining_time + 1) / 2;
+    let geodes_maximum_bound =
+        resources.geode + robot_counts.geode * remaining_time + triangle_number;
+    if geodes_maximum_bound < *best_geodes_so_far {
+        return 0;
+    }
+
+    // Geode is the final goal: we should build it everytime it's possible to do so
+    if let Some(resources) = resources.paying(blueprint.geode_robot_cost) {
+        return search(
+            blueprint,
+            resources + robot_counts,
+            robot_counts.adding(Resource::Geode),
+            remaining_time - 1,
+            best_geodes_so_far,
+        );
+    }
+
+    let mut best_geodes = 0;
+
+    // Explode the consequence of building each possible robot now
+    let mut can_pay_all = true;
+    if let Some(resources) = resources.paying(blueprint.obsidian_robot_cost) {
+        if robot_counts.obsidian < blueprint.max_obsidian_robots {
+            best_geodes = best_geodes.max(search(
+                blueprint,
+                resources + robot_counts,
+                robot_counts.adding(Resource::Obsidian),
+                remaining_time - 1,
+                best_geodes_so_far,
+            ));
+        }
+    } else {
+        can_pay_all = false;
+    }
+    if let Some(resources) = resources.paying(blueprint.clay_robot_cost) {
+        if robot_counts.clay < blueprint.max_clay_robots {
+            best_geodes = best_geodes.max(search(
+                blueprint,
+                resources + robot_counts,
+                robot_counts.adding(Resource::Clay),
+                remaining_time - 1,
+                best_geodes_so_far,
+            ));
+        }
+    } else {
+        can_pay_all = false;
+    }
+    if let Some(resources) = resources.paying(blueprint.ore_robot_cost) {
+        if robot_counts.ore < blueprint.max_ore_robots {
+            best_geodes = best_geodes.max(search(
+                blueprint,
+                resources + robot_counts,
+                robot_counts.adding(Resource::Ore),
+                remaining_time - 1,
+                best_geodes_so_far,
+            ));
+        }
+    } else {
+        can_pay_all = false;
+    }
+
+    // Explode the consequence of not building anything now
+    if !can_pay_all {
+        best_geodes = best_geodes.max(search(
+            blueprint,
+            resources + robot_counts,
+            robot_counts,
+            remaining_time - 1,
+            best_geodes_so_far,
+        ));
+    }
+
+    best_geodes
+}
+
+impl Blueprint {
+    pub fn new(
+        id: i32,
+        ore_robot_cost: Resources,
+        clay_robot_cost: Resources,
+        obsidian_robot_cost: Resources,
+        geode_robot_cost: Resources,
+    ) -> Self {
+        let costs = [
+            ore_robot_cost,
+            clay_robot_cost,
+            obsidian_robot_cost,
+            geode_robot_cost,
+        ];
+
+        Self {
+            id,
+            ore_robot_cost,
+            clay_robot_cost,
+            obsidian_robot_cost,
+            geode_robot_cost,
+            max_ore_robots: costs.iter().map(|cost| cost.ore).max().unwrap(),
+            max_clay_robots: costs.iter().map(|cost| cost.clay).max().unwrap(),
+            max_obsidian_robots: costs.iter().map(|cost| cost.obsidian).max().unwrap(),
+        }
+    }
+}
+
 impl Resources {
     const ZERO: Self = Self {
         ore: 0,
@@ -116,5 +269,35 @@ impl Resources {
             Resource::Obsidian => &mut self.obsidian,
             Resource::Geode => &mut self.geode,
         }
+    }
+
+    fn paying(self, cost: Self) -> Option<Self> {
+        if self.ore >= cost.ore
+            && self.clay >= cost.clay
+            && self.obsidian >= cost.obsidian
+            && self.geode >= cost.geode
+        {
+            Some(self - cost)
+        } else {
+            None
+        }
+    }
+
+    fn adding(mut self, resource: Resource) -> Self {
+        match resource {
+            Resource::Ore => {
+                self.ore += 1;
+            }
+            Resource::Clay => {
+                self.clay += 1;
+            }
+            Resource::Obsidian => {
+                self.obsidian += 1;
+            }
+            Resource::Geode => {
+                self.geode += 1;
+            }
+        }
+        self
     }
 }
